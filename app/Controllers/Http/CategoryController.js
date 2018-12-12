db214930252e76916aa2c07acf6c44529d2fde82
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:54:15 -0200

    Adicionado controller de categoria das receitas e despesas

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Category = use('App/Models/Category')
+
 class CategoryController {
+
+  async index ({ request, response, view, auth }) {
+    return await Category.all()
+  }
+
+  async store ({ request, response, auth }) {
+    const data = request.all()
+    return await Category.create(data)
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await Category.findOrFail(params.id)
+  }
+
+  async update ({ params, request, response, auth }) {
+    const data = request.all()
+    const category = await Category.findOrFail(params.id)
+    category.merge(data)
+    await category.save()
+    return category
+  }
+
+  async destroy ({ params, request, response, auth }) {
+    const category = await Category.findOrFail(params.id)
+    await category.delete()
+  }
+
 }
 
 module.exports = CategoryController
