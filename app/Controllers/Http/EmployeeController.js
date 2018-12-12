768a87464b0943b71dc2ae3047f03d56ef70e494
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:54:58 -0200

    Adicionado controller de empregado

--- app/Controllers/Http/EmployeeController.js
+++ app/Controllers/Http/EmployeeController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Employee = use('App/Models/Employee')
+
 class EmployeeController {
+
+  async index ({ request, response, view, auth }) {
+    return await Employee.all()
+  }
+
+  async store ({ request, response, auth }) {
+    const data = request.all()
+    return await Employee.create(data)
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await Employee.findOrFail(params.id)
+  }
+
+  async update ({ params, request, response, auth }) {
+    const data = request.all()
+    const employee = await Employee.findOrFail(params.id)
+    employee.merge(data)
+    await employee.save()
+    return employee
+  }
+
+  async destroy ({ params, request, response, auth }) {
+    const employee = await Employee.findOrFail(params.id)
+    await employee.delete()
+  }
+
 }
 
 module.exports = EmployeeController
