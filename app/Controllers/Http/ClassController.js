521fb42b148a3ebdb644e70beb2e15da73b71640
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:54:50 -0200

    Adicionado controller de classe do aluno

--- app/Controllers/Http/ClassController.js
+++ app/Controllers/Http/ClassController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Class = use('App/Models/Class')
+
 class ClassController {
+
+  async index ({ request, response, view, auth }) {
+    return await Class.all()
+  }
+
+  async store ({ request, response, auth }) {
+    const data = request.all()
+    return await Class.create(data)
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await Class.findOrFail(params.id)
+  }
+
+  async update ({ params, request, response, auth }) {
+    const data = request.all()
+    const classs = await Class.findOrFail(params.id)
+    classs.merge(data)
+    await classs.save()
+    return classs
+  }
+
+  async destroy ({ params, request, response, auth }) {
+    const classs = await Class.findOrFail(params.id)
+    await classs.delete()
+  }
+
 }
 
 module.exports = ClassController
