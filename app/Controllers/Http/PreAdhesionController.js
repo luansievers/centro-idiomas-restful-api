03dd0d34873eae7d2dcbf5c15df1be9d5548eaa8
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:56:03 -0200

    Adicionado controller de preadesão

--- app/Controllers/Http/PreAdhesionController.js
+++ app/Controllers/Http/PreAdhesionController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const PreAdhesion = use('App/Models/PreAdhesion')
+
 class PreAdhesionController {
+
+  async index ({ request, response, view, auth }) {
+    return await PreAdhesion.all()
+  }
+
+  async store ({ request, response, auth }) {
+    const data = request.all()
+    return await PreAdhesion.create(data)
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await PreAdhesion.findOrFail(params.id)
+  }
+
+  async update ({ params, request, response, auth }) {
+    const data = request.all()
+    const preAdhesion = await PreAdhesion.findOrFail(params.id)
+    preAdhesion.merge(data)
+    await preAdhesion.save()
+    return preAdhesion
+  }
+
+  async destroy ({ params, request, response, auth }) {
+    const preAdhesion = await PreAdhesion.findOrFail(params.id)
+    await preAdhesion.delete()
+  }
+
 }
 
 module.exports = PreAdhesionController
