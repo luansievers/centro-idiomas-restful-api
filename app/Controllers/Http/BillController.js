f2a8bda71c09aa29bf68c8ffa68e88cff48ad1e4
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:43:31 -0200

    Adicionado controller de parcelas a pagar do aluno

--- app/Controllers/Http/BillController.js
+++ app/Controllers/Http/BillController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Bill = use('App/Models/Bill')
+
 class BillController {
+
+    async index({ request, response, view, auth }) {
+        return await Bill.all()
+    }
+
+    async store({ request, response, auth }) {
+        const data = request.all()
+        return await Bill.create(data)
+    }
+
+    async show({ params, request, response, view, auth }) {
+        return await Bill.findOrFail(params.id)
+    }
+
+    async update({ params, request, response, auth }) {
+        const data = request.all()
+        const bill = await Bill.findOrFail(params.id)
+        bill.merge(data)
+        await bill.save()
+        return bill
+    }
+
+    async destroy({ params, request, response, auth }) {
+        const bill = await Bill.findOrFail(params.id)
+        await bill.delete()
+    }
+
 }
 
 module.exports = BillController
