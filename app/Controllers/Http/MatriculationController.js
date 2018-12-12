7f3bc136de746e27d51b502ce1964b575e25c4f5
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:55:43 -0200

    Adicionado controller da matricula do aluno

--- app/Controllers/Http/MatriculationController.js
+++ app/Controllers/Http/MatriculationController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Matriculation = use('App/Models/Matriculation')
+
 class MatriculationController {
+
+  async index ({ request, response, view, auth }) {
+    return await Matriculation.all()
+  }
+
+  async store ({ request, response, auth }) {
+    const data = request.all()
+    return await Matriculation.create(data)
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await Matriculation.findOrFail(params.id)
+  }
+
+  async update ({ params, request, response, auth }) {
+    const data = request.all()
+    const matriculation = await Matriculation.findOrFail(params.id)
+    matriculation.merge(data)
+    await matriculation.save()
+    return matriculation
+  }
+
+  async destroy ({ params, request, response, auth }) {
+    const matriculation = await Matriculation.findOrFail(params.id)
+    await matriculation.delete()
+  }
+
 }
 
 module.exports = MatriculationController
