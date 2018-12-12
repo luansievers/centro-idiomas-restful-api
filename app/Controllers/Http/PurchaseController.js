650bd99fcbf7c67f5aa68d8521ca6a42525cc745
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:56:32 -0200

    Adicionado controller de compras do produto

--- app/Controllers/Http/PurchaseController.js
+++ app/Controllers/Http/PurchaseController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Purchase = use('App/Models/Purchase')
+
 class PurchaseController {
+
+  async index ({ request, response, view, auth }) {
+    return await Purchase.all()
+  }
+
+  async store ({ request, response, auth }) {
+    const data = request.all()
+    return await Purchase.create(data)
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await Purchase.findOrFail(params.id)
+  }
+
+  async update ({ params, request, response, auth }) {
+    const data = request.all()
+    const purchase = await Purchase.findOrFail(params.id)
+    purchase.merge(data)
+    await purchase.save()
+    return purchase
+  }
+
+  async destroy ({ params, request, response, auth }) {
+    const purchase = await Purchase.findOrFail(params.id)
+    await purchase.delete()
+  }
+
 }
 
 module.exports = PurchaseController
