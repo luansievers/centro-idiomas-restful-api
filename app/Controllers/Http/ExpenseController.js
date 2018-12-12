d039492674d65d4e64aaa40857fc6e320bc5fd0a
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:55:16 -0200

    Adicionado controller de despesas

--- app/Controllers/Http/ExpenseController.js
+++ app/Controllers/Http/ExpenseController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Expense = use('App/Models/Expense')
+
 class ExpenseController {
+    
+  async index ({ request, response, view, auth }) {
+    return await Expense.all()
+  }
+
+  async store ({ request, response, auth }) {
+    const data = request.all()
+    return await Expense.create(data)
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await Expense.findOrFail(params.id)
+  }
+
+  async update ({ params, request, response, auth }) {
+    const data = request.all()
+    const expense = await Expense.findOrFail(params.id)
+    expense.merge(data)
+    await expense.save()
+    return expense
+  }
+
+  async destroy ({ params, request, response, auth }) {
+    const expense = await Expense.findOrFail(params.id)
+    await expense.delete()
+  }
+
 }
 
 module.exports = ExpenseController
