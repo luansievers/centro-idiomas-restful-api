2192c4004c4ff080ff03a56285e911c881817e57
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:55:09 -0200

    Adicionado controller de evento da agenda

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Event = use('App/Models/Event')
+
 class EventController {
+
+  async index ({ request, response, view, auth }) {
+    return await Event.all()
+  }
+
+  async store ({ request, response, auth }) {
+    const data = request.all()
+    return await Event.create(data)
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await Event.findOrFail(params.id)
+  }
+
+  async update ({ params, request, response, auth }) {
+    const data = request.all()
+    const event = await Event.findOrFail(params.id)
+    event.merge(data)
+    await event.save()
+    return event
+  }
+
+  async destroy ({ params, request, response, auth }) {
+    const event = await Event.findOrFail(params.id)
+    await event.delete()
+  }
+
 }
 
 module.exports = EventController
