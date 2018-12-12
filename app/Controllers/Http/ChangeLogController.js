710e608d98680c79e1be1ca7dd00b01d7a15a6b2
Luan Felipe Sievers <luanfelipesievers@gmail.com>
2018-12-11 23:54:38 -0200

    Adicionado controller dos logs da aplicação para visualizacao

--- app/Controllers/Http/ChangeLogController.js
+++ app/Controllers/Http/ChangeLogController.js
@@ -1,6 +1,16 @@
 'use strict'
 
+const ChangeLog = use('App/Models/ChangeLog')
+
 class ChangeLogController {
+
+  async index ({ request, response, view, auth }) {
+    return await ChangeLog.all()
+  }
+
+  async show ({ params, request, response, view, auth }) {
+    return await ChangeLog.findOrFail(params.id)
+  }
 }
 
 module.exports = ChangeLogController
